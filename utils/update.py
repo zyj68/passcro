040d4d4ff556fd36f9ddc7d74bff041aac891664
zhangkaiitugithub <49013990+zhangkaiitugithub@users.noreply.github.com>
2022-06-22 10:32:09 +0800

    Update update.py

--- utils/update.py
+++ utils/update.py
@@ -11,22 +11,13 @@ import os,time,requests,base64
 
 hdrs = {'User-Agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Mobile Safari/537.36'}
 
-def safe_decode(s):
-    ori = s
-    try:
-        s = s + '=' * (4 - len(s) % 4) if len(s) % 4 else s
-        s = base64.urlsafe_b64decode(s).decode()
-    except Exception as er:
-        s = ori
-    return s
 
 
-
-def easyclash():
+def easy():
 
     try:
         ctnt = requests.get('https://api.buliang0.cf/easyclash')
-        yamltent = safe_decode(ctnt.text)
+        yamltent = base64.urlsafe_b64decode(ctnt.text).decode()
         dirs = './subs'
         if not os.path.exists(dirs):
             os.makedirs(dirs)
@@ -38,7 +29,7 @@ def easyclash():
             time.sleep(2)
             try:
                 ctnt = requests.get('https://api.buliang0.cf/easyclash')
-                yamltent = safe_decode(ctnt.text)
+                yamltent = base64.urlsafe_b64decode(ctnt.text).decode()
                 dirs = './subs'
                 if not os.path.exists(dirs):
                     os.makedirs(dirs)
@@ -50,17 +41,19 @@ def easyclash():
                     time.sleep(2)
                     try:
                         ctnt = requests.get('https://api.buliang0.cf/easyclash')
-                        yamltent = safe_decode(ctnt.text)
+                        yamltent = base64.urlsafe_b64decode(ctnt.text).decode()
                         dirs = './subs'
                         if not os.path.exists(dirs):
                             os.makedirs(dirs)
                         if rsp.status_code == 200:
                             with open(dirs + '/' + 'easy.yaml', 'w', encoding='utf-8') as f:
                                 f.write(yamltent)
-
+                    except Exception:
+                        pass
 
 
 def pjzy():
+
     url = 'https://raw.githubusercontent.com/pojiezhiyuanjun/freev2/master/'+time.strftime('%m%d')+'clash.yml'
     try:
         rsp = requests.get(url,headers = hdrs)
@@ -73,7 +66,7 @@ def pjzy():
                 f.write(yamltent)
     except Exception:
         pass
-
+    
 def miao():
     url = 'https://raw.githubusercontent.com/Strongmiao168/Clash/main/'+time.strftime('%m%d')+'clash.yml'
     try:
@@ -92,6 +85,6 @@ def miao():
 if __name__ == '__main__':
 
     pjzy()
-    easyclash()
+    easy()
     miao()
 
